f25f6360c2205ae5abf15e27653866271ce0fd82
Sergio Flores <sergioflores836@gmail.com>
2015-09-25 15:00:16 -0500

    started the loop for api calls. Having trouble accessing the index of the streams during the success function

--- js/main.js
+++ js/main.js
@@ -1,17 +1,24 @@
 $(document).ready(function(){
 	$('#status').text("jquery is working");
-	var streamName = 'freecodecamp';
-	$.ajax({
+	var streamName = ["freecodecamp", "storbeck", "medryBW"];
+	for(i = 0; i<streamName.length; i++){
+		console.log(streamName[i]);
+		$.ajax({
 		type: 'GET',
-		url: 'https://api.twitch.tv/kraken/streams/' + streamName,
+		url: 'https://api.twitch.tv/kraken/streams/' + streamName[i],
 		success: function(data){
-			console.log(data.stream);
+			//console.log(data);
+			var streamName2 = ["freecodecamp", "storbeck", "medryBW"];
+			console.log(streamName2[i-2]);
+
 			if(data.stream === null){
-				$('#list').append("<li>" + streamName + " is offline</li>");	
+				$('#list').append("<li>" + streamName[i-1] + " is offline</li>");	
 			}
 			else{
-				$('#list').append("<li>" + streamName + " is online</li>");
+				$('#list').append("<li>" + streamName[i-1] + " is online</li>");
 			}
 		}
-	});
+		});
+	}
+	
 });

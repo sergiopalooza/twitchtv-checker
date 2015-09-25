32af20e6aafeeeb06a3ef3a56bfeef71088e0668
Sergio Flores <sergioflores836@gmail.com>
2015-09-25 17:26:59 -0500

    figured out how to correctly make all of the calls and disply names

--- js/main.js
+++ js/main.js
@@ -1,21 +1,22 @@
 $(document).ready(function(){
 	$('#status').text("jquery is working");
-	var streamName = ["freecodecamp", "storbeck", "medryBW"];
-	for(i = 0; i<streamName.length; i++){
-		console.log(streamName[i]);
+	var streamName = ["medrybw", "freecodecamp", "storbeck", "kolento"];
+	
+	for(i=0; i<streamName.length; i++){
 		$.ajax({
 		type: 'GET',
 		url: 'https://api.twitch.tv/kraken/streams/' + streamName[i],
 		success: function(data){
-			//console.log(data);
-			var streamName2 = ["freecodecamp", "storbeck", "medryBW"];
-			console.log(streamName2[i-2]);
+			console.log(data);
+			i--; //I have no idea why I need to do this but decrementing it here makes the index work properly (in reverse)
+			var streamUrl = data._links.channel;
+			var name =  /([^/]+$)/g.exec(streamUrl);
 
 			if(data.stream === null){
-				$('#list').append("<li>" + streamName[i-1] + " is offline</li>");	
+				$('#list').append("<div class='off'>" + name[0] + " is offline</div>");	
 			}
 			else{
-				$('#list').append("<li>" + streamName[i-1] + " is online</li>");
+				$('#list').append("<div class='on'>" + name[0] + " is online</div>");
 			}
 		}
 		});
